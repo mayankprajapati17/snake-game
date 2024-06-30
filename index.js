69182e7027035a7695a7bde02aa0712bc3e0cfe8
Mayank Prajapati <mayankprajapati2703@gmail.com>
2024-06-30 22:26:20 +0530

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+let direction ={x:0,y:0};
+let foodsound=new Audio('food.mp3');
+let gameoversound=new Audio('gameover.mp3');
+let movesound= new Audio('move.mp3');
+let musicsound=new Audio('music.mp3');
